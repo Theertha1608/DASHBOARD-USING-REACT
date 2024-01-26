c064890cf4c291687feecc4b13b1b4eff8a56c20
theetha1608 <theerthakunjusss@gmail.com>
2024-01-26 16:43:17 +0530

    imagebox fixed

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -1,57 +1,63 @@
-import React from 'react';
-import mediaImage from '../Assets/media.png';
-import facebookImage from '../Assets/facebook.png';
-import instagramImage from '../Assets/instagram.png';
-import twitterImage from '../Assets/twitter.png';
+import React from "react";
+import mediaImage from "../Assets/media.png";
+import facebookImage from "../Assets/facebook.png";
+import instagramImage from "../Assets/instagram.png";
+import twitterImage from "../Assets/twitter.png";
+import { Box, Card, Grid, CardContent, Typography } from "@mui/material";
 
 const ImageBox = () => {
-  const boxStyle = {
-    width: '200px',
-    height: '300px',
-    backgroundColor: '#ffffff',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-    position: 'fixed',
-    bottom: '10px',
-    right: '0px',
-    borderRadius: '10px',
-    backgroundImage: `url(${'./'})`,
-    backgroundSize: 'cover',
-  };
-
-  const nameStyle = {
-    color: 'black',
-    marginTop: '10px',
-    textAlign: 'center',
-  };
-
-  const positionStyle = {
-    textAlign: 'center',
-  };
-
-  const socialMediaStyle = {
-    position: 'absolute',
-    bottom: '5px',
-    width: '100%',
-    textAlign: 'center',
-  };
-
-  const socialMediaIconStyle = {
-    width: '15px',
-    height: '15px', 
-    margin: '0 10px', 
-  };
-
   return (
-    <div style={boxStyle}>
-      <img src={mediaImage} alt="media.png" />
-      <div style={nameStyle}>John Doe</div>
-      <div style={positionStyle}>CEO</div>
-      <div style={socialMediaStyle}>
-        <img src={facebookImage} alt="Facebook" style={socialMediaIconStyle} />
-        <img src={instagramImage} alt="Instagram" style={socialMediaIconStyle} />
-        <img src={twitterImage} alt="Twitter" style={socialMediaIconStyle} />
-      </div>
-    </div>
+    <Box sx={{ marginLeft: "252px", width: "400px" }}>
+      <Card
+        sx={{
+          width: "50%",
+          marginLeft: "152px",
+          borderRadius: 3,
+          boxShadow: 6,
+        }}
+      >
+        <Grid container direction="column">
+          <Grid item>
+          
+            <img src={mediaImage} alt="media.png" style={{ width: "100%" }} />
+          </Grid>
+          <Grid item>
+         
+            <CardContent>
+              <Typography variant="h6" component="div">
+                John Doe
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                CEO
+              </Typography>
+              <Grid container justifyContent="center" spacing={2}>
+                <Grid item>
+                  <img
+                    src={facebookImage}
+                    alt="Facebook"
+                    style={{ width: "15px", height: "15px" }}
+                  />
+                </Grid>
+                <Grid item>
+                  <img
+                    src={instagramImage}
+                    alt="Instagram"
+                    style={{ width: "15px", height: "15px" }}
+                  />
+                </Grid>
+                <Grid item>
+                  <img
+                    src={twitterImage}
+                    alt="Twitter"
+                    style={{ width: "15px", height: "15px" }}
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </Card>
+    </Box>
   );
 };
 
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Pie } from "react-chartjs-2";
+import { Card } from "@mui/material";
 
 const PieChart = () => {
   const [pieChartData, setPieChartData] = useState([]);
@@ -18,21 +19,54 @@ const PieChart = () => {
       {
         data: pieChartData.map((item) => item.value),
         backgroundColor: [
-          // "rgba(255, 99, 132, 0.6)",
-          // "rgba(54, 162, 235, 0.6)",
-          // "rgba(255, 206, 86, 0.6)",
-          // "rgba(75, 192, 192, 0.6)",
-          // "rgba(153, 102, 255, 0.6)",
+          "rgba(136, 194, 140)",
+          "rgba(216, 240, 217)",
+          "rgba(193, 227, 194)",
+          "rgba(184, 245, 185)",
+          "rgba(160, 250, 161)",
         ],
+        radius: "70%", // Set the radius to control the size of circles
       },
     ],
   };
 
-  return; (
-   <div>
-    <h2>Pie Chart</h2>
-     { <Pie data={data} /> }
-   </div>
+  const options = {
+    plugins: {
+      legend: {
+        position: "bottom", // or "top", "left", "right" for other positions
+        align: "start", // or "start", "end" for alignment within the position
+
+        labels: {
+          usePointStyle: true,
+          boxWidth: 12,
+          font: {
+            size: 14, // set the font size for legend labels
+            weight: "bold",
+          },
+          padding: 6, // set the padding for legend labels
+        },
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false, // Set to false to allow chart resizing
+  };
+
+  return (
+    <>
+      <Card
+        sx={{
+          marginLeft: 44,
+          height: 290,
+          width: 350,
+          mt: 6,
+          boxShadow: 6,
+          borderRadius: 3,
+        }}
+      >
+        {" "}
+        <Pie data={data} options={options} />
+      </Card>
+    </>
   );
 };
 
