b08b503a4b566bfa66bc410ec743383c070fe1cd
theetha1608 <theerthakunjusss@gmail.com>
2024-01-26 16:24:14 +0530

    modified Layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,78 +1,43 @@
-import React from 'react';
-import Linechart from './Linechart';
-import PieChart from './PieChart';
-import Table from './Table';
-import ImageBox from './ImageBox';
-import Profile from './Profile';
-import Pagination from './Pagination'; // Import the Pagination component
+import React from "react";
+import { Grid } from "@mui/material";
+import Linechart from "./Linechart";
+import PieChart from "./PieChart";
+import Table from "./Table";
+import ImageBox from "./ImageBox";
+import Profile from "./Profile";
+import Pagination from "./Pagination";
 
 const Home = () => {
-  const homeContainerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'justifyContent',
-    justifyContent: 'space-between',
-    height: '100vh',
-    padding: '20px',
-  };
-
-  const greetingStyle = {
-    color: '#000000',
-    fontSize: '1.2em',
-    fontWeight: 'bold',
-  };
-
-  const sunEmojiStyle = {
-    marginRight: '0.5em',
-  };
-
-  const chartsContainerStyle = {
-    display: 'flex',
-    flexGrow: 1,
-    width: '100%',
-  };
-
-  const chartsLeftStyle = {
-    flexGrow: 1,
-  };
-
-  const chartsRightStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
-  };
-
-  const imageBoxContainerStyle = {
-    alignSelf: 'flex-end',
-  };
-
   const handlePageChange = (newPage) => {
-    // Handle page change logic here (e.g., fetching data for the new page)
     console.log(`Page changed to ${newPage}`);
   };
 
   return (
-    <div style={homeContainerStyle}>
+    <div>
       <Profile />
-      <div style={greetingStyle}>
-        Good Morning!
-        <span role="img" aria-label="sun emoji" style={sunEmojiStyle}>
-          ☀️
-        </span>
-      </div>
-      <div style={chartsContainerStyle}>
-        <div style={chartsLeftStyle}>
-          <Table />
-        </div>
-        <div style={chartsRightStyle}>
+      <h2 style={{ color: "black", marginLeft: "24px" }}>Good Morning! ☀️</h2>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
           <Linechart />
+        </Grid>
+        <Grid item xs={12} md={6}>
           <PieChart />
-        </div>
-      </div>
-      <div style={imageBoxContainerStyle}>
-        <ImageBox />
-      </div>
-      <Pagination totalPages={10} currentPage={1} onPageChange={handlePageChange} />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Table />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ImageBox />
+        </Grid>
+      </Grid>
+
+      {/* <Pagination
+        totalPages={10}
+        currentPage={1}
+        onPageChange={handlePageChange}
+      /> */}
     </div>
   );
 };
